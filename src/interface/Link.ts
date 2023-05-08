887d4d5bdd6ce42a3a026c9e993b3a6d58284c95
Henning Norden <heno@henomedia.de>
2023-05-08 23:36:16 +0200

    v1.0.4

--- src/interface/Link.ts
+++ src/interface/Link.ts
@@ -1,8 +1,10 @@
+import React = require('react');
+
 export interface Link {
   key: string;
   name: string;
   categoryKey: string;
-  component?: HTMLElement | string;
+  component?: HTMLElement | string | React.DetailedReactHTMLElement<React.HTMLAttributes<HTMLElement>, HTMLElement>;
   description?: string;
   path?: string;
   icon?: string;
